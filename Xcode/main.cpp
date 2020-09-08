2e8caa89f3f45d565ec670a6b88ee3844fea589e
Yingxin Xie <yingxix@uci.edu>
2020-09-08 10:22:34 +0800

    Initial Commit

---
+++ Xcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Xcode
+//
+//  Created by 谢迎昕 on 9/8/20.
+//  Copyright © 2020 谢迎昕. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
